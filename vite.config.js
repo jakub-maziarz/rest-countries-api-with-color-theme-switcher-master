c9315175ebf514b3a6b66fa44605151ab18705d4
jakub-maziarz <jakub.maziarz@outlook.com>
2023-02-06 01:15:41 +0100

    Revert "Edit Vite config"
    This reverts commit 76d026a9e1d0b352c3e3258fd88af59802c55bc1.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/rest-countries-api-with-color-theme-switcher-master/",
-    plugins: [vue()],
-});
+  plugins: [vue()],
+})
